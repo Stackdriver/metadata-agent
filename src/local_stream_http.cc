64820e7823d5a5f125eeb876c6fb3184dc9e82ee
Igor Peshansky <igorpeshansky@github.com>
2017-05-09 11:56:47 -0400

    License headers.

--- src/local_stream_http.cc
+++ src/local_stream_http.cc
@@ -1,3 +1,9 @@
+// Copyright 2017 Igor Peshansky (igorp@google.com).
+// Copyright 2017 Google, Inc.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
 #include "local_stream_http.h"
 
 namespace boost { namespace network { namespace http { namespace impl {
--- src/local_stream_http.h
+++ src/local_stream_http.h
@@ -1,7 +1,9 @@
 #ifndef LOCAL_STREAM_HTTP
 #define LOCAL_STREAM_HTTP
 
-// Copyright Dean Michael Berris 2010.
+// Copyright 2010 Dean Michael Berris.
+// Copyright 2017 Igor Peshansky (igorp@google.com).
+// Copyright 2017 Google, Inc.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
