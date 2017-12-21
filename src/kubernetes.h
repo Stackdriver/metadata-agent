6705868b24aadeedc9c685fe27d16f21e4cbdea0
Igor Peshansky <igorpeshansky@github.com>
2017-12-20 19:19:21 -0500

    Since containerStatuses and containerSpecs may not be in the same order, look up containers by name, rather than by index.

--- src/kubernetes.h
+++ src/kubernetes.h
@@ -89,12 +89,13 @@ class KubernetesReader {
       const throw(json::Exception);
   // Given a pod object and container index, return the container metadata.
   MetadataUpdater::ResourceMetadata GetContainerMetadata(
-      const json::Object* pod, int container_index, json::value associations,
+      const json::Object* pod, const json::Object* container_status,
+      const json::Object* container_spec, json::value associations,
       Timestamp collected_at) const throw(json::Exception);
   // Given a pod object and container index, return the legacy resource.
   // The returned "metadata" field will be Metadata::IGNORED.
   MetadataUpdater::ResourceMetadata GetLegacyResource(
-      const json::Object* pod, int container_index) const
+      const json::Object* pod, const std::string& container_name) const
       throw(json::Exception);
   // Given a pod object, return the associated pod and container metadata.
   std::vector<MetadataUpdater::ResourceMetadata> GetPodAndContainerMetadata(
