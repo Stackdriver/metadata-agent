626e1d7929dfd095617d49f78014f2111953abf5
Supriya Garg <supriyagarg@google.com>
2018-03-01 18:45:42 -0500

    Address comments

--- test/format_unittest.cc
+++ test/format_unittest.cc
@@ -4,24 +4,24 @@
 namespace {
 
 TEST(SubstituteTest, SingleSubstitution) {
-  const std::string format_str("prefix|{{subst}}|postfix");
+  const std::string format_str("prefix|{{subst}}|suffix");
   EXPECT_EQ(
-      "prefix|value|postfix",
+      "prefix|value|suffix",
       format::Substitute(format_str, {{"subst", "value"}})
   );
 }
 
 TEST(SubstituteTest, MultipleSubstitutions) {
-  const std::string format_str("prefix|{{subst}}|{{another_subst}}|postfix");
+  const std::string format_str("prefix|{{first}}|middle|{{second}}|suffix");
   EXPECT_EQ(
-      "prefix|value|value2|postfix",
+      "prefix|one|middle|two|suffix",
       format::Substitute(
-          format_str, {{"subst", "value"}, {"another_subst", "value2"}})
+          format_str, {{"first", "one"}, {"second", "two"}})
   );
 }
 
 TEST(SubstituteTest, UnterminatedPlaceholder) {
-  const std::string format_str("prefix|{{subst|postfix");
+  const std::string format_str("prefix|{{subst|suffix");
   EXPECT_THROW(
       format::Substitute(format_str, {{"subst", "value"}}),
       format::Exception
@@ -29,9 +29,9 @@ TEST(SubstituteTest, UnterminatedPlaceholder) {
 }
 
 TEST(SubstituteTest, UnknownParameter) {
-  const std::string format_str("prefix|{{subst}}|postfix");
+  const std::string format_str("prefix|{{subst}}|suffix");
   EXPECT_THROW(
-      format::Substitute(format_str, {{"another_subst", "value"}}),
+      format::Substitute(format_str, {{"unknown", "value"}}),
       format::Exception
   );
 }
