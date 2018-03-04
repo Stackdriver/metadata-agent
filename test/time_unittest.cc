f715f2dd289dceb64129101089bb1ca678cd141f
Igor Peshansky <igorpeshansky@github.com>
2018-03-04 00:15:07 -0500

    Even more tests.

--- test/time_unittest.cc
+++ test/time_unittest.cc
@@ -58,4 +58,85 @@ TEST(TimeTest, FromStringMoreDigits) {
   );
 }
 
+TEST(TimeTest, FromStringPositiveNanos) {
+  const std::chrono::system_clock::time_point t =
+      rfc3339::FromString("2018-03-03T01:23:45.+678901234Z");
+  EXPECT_EQ(
+      "1970-01-01T00:00:00.000000000Z",
+      rfc3339::ToString(t)
+  );
+}
+
+TEST(TimeTest, FromStringNegativeNanos) {
+  const std::chrono::system_clock::time_point t =
+      rfc3339::FromString("2018-03-03T01:23:45.-678901234Z");
+  EXPECT_EQ(
+      "1970-01-01T00:00:00.000000000Z",
+      rfc3339::ToString(t)
+  );
+}
+
+TEST(TimeTest, FromStringPositiveSeconds) {
+  const std::chrono::system_clock::time_point t =
+      rfc3339::FromString("2018-03-03T01:23:+45.678901234Z");
+  EXPECT_EQ(
+      "1970-01-01T00:00:00.000000000Z",
+      rfc3339::ToString(t)
+  );
+}
+
+TEST(TimeTest, FromStringNegativeSeconds) {
+  const std::chrono::system_clock::time_point t =
+      rfc3339::FromString("2018-03-03T01:23:-45.678901234Z");
+  EXPECT_EQ(
+      "1970-01-01T00:00:00.000000000Z",
+      rfc3339::ToString(t)
+  );
+}
+
+TEST(TimeTest, FromStringHexSeconds) {
+  const std::chrono::system_clock::time_point t =
+      rfc3339::FromString("2018-03-03T01:23:0x45.678901234Z");
+  EXPECT_EQ(
+      "1970-01-01T00:00:00.000000000Z",
+      rfc3339::ToString(t)
+  );
+}
+
+TEST(TimeTest, FromStringInfSeconds) {
+  const std::chrono::system_clock::time_point t1 =
+      rfc3339::FromString("2018-03-03T01:23:+infZ");
+  EXPECT_EQ(
+      "1970-01-01T00:00:00.000000000Z",
+      rfc3339::ToString(t1)
+  );
+  const std::chrono::system_clock::time_point t2 =
+      rfc3339::FromString("2018-03-03T01:23:-infZ");
+  EXPECT_EQ(
+      "1970-01-01T00:00:00.000000000Z",
+      rfc3339::ToString(t2)
+  );
+  const std::chrono::system_clock::time_point t3 =
+      rfc3339::FromString("2018-03-03T01:23:+nanZ");
+  EXPECT_EQ(
+      "1970-01-01T00:00:00.000000000Z",
+      rfc3339::ToString(t3)
+  );
+  const std::chrono::system_clock::time_point t4 =
+      rfc3339::FromString("2018-03-03T01:23:-nanZ");
+  EXPECT_EQ(
+      "1970-01-01T00:00:00.000000000Z",
+      rfc3339::ToString(t4)
+  );
+}
+
+TEST(TimeTest, FromStringTooManySeconds) {
+  const std::chrono::system_clock::time_point t =
+      rfc3339::FromString("2018-03-03T01:23:1045.678901234Z");
+  EXPECT_EQ(
+      "1970-01-01T00:00:00.000000000Z",
+      rfc3339::ToString(t)
+  );
+}
+
 }
