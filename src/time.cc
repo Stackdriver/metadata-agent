1e29f74d6f8da87691c627f7b3915a70c9c1f1f6
igorpeshansky <igorpeshansky@users.noreply.github.com>
2018-03-13 11:15:15 -0400

    Fix parsing of fractional seconds. (#65)

--- src/time.cc
+++ src/time.cc
@@ -16,6 +16,8 @@
 
 #include "time.h"
 
+#include <cctype>
+#include <cmath>
 #include <cstdlib>
 #include <iomanip>
 #include <mutex>
@@ -31,8 +33,11 @@ namespace {
 // See http://stackoverflow.com/questions/4137748.
 int UTCOffset() {
   const std::time_t local = std::time(nullptr);
-  const std::time_t utc = std::mktime(std::gmtime(&local));
-  return utc - local;
+  std::tm utc_time = safe_gmtime(&local);
+  // Since we're only using this for conversion to UTC, always turn off DST.
+  utc_time.tm_isdst = 0;
+  const std::time_t utc = std::mktime(&utc_time);
+  return local - utc;
 }
 
 const int kUtcOffset = UTCOffset();
@@ -57,17 +62,36 @@ std::string ToString(const std::chrono::system_clock::time_point& t) {
 
 std::chrono::system_clock::time_point FromString(const std::string& s) {
   std::tm tm;
-  const char* end = strptime(s.c_str(), "%Y-%m-%dT%H:%M:%S", &tm);
-  if (end == nullptr || end - s.c_str() != s.find('.')) {
-    // TODO
+  char* const end = strptime(s.c_str(), "%Y-%m-%dT%H:%M:%S", &tm);
+  if (end == nullptr || !std::isdigit(*(end - 2))) {
+    // TODO: Invalid time format.
     return std::chrono::system_clock::time_point();
   }
   char* zone;
-  long ns = std::strtol(end + 1, &zone, 10);
-  if (zone <= end + 1 || *zone != 'Z' || *(zone+1) != '\0') {
-    // TODO
+  if (*end == '.') {
+    (void)std::strtol(end + 1, &zone, 10);
+    if (zone <= end + 1) {
+      // TODO: Missing nanoseconds.
+      return std::chrono::system_clock::time_point();
+    }
+  } else {
+    zone = end;
+  }
+  if (*zone != 'Z' || *(zone+1) != '\0') {
+    // TODO: Invalid timezone.
     return std::chrono::system_clock::time_point();
   }
+  char* d_end;
+  double seconds = std::strtod(end - 2, &d_end);
+  if (d_end != zone) {
+    // TODO: Internal error.
+    return std::chrono::system_clock::time_point();
+  }
+  static_assert(sizeof(long) == 8, "long is too small");
+  // Truncate to 9 digits by rounding to 10 and discarding the last one.
+  long ns = std::lround((seconds - tm.tm_sec) * 10000000000) / 10;
+  // Our UTC offset constant assumes no DST.
+  tm.tm_isdst = 0;
   const std::time_t local_time = std::mktime(&tm);
   const std::time_t utc_time = local_time + kUtcOffset;
   std::chrono::system_clock::time_point sec =
