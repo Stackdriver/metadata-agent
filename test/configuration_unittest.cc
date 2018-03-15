4b55cda546b32ac183ea19c9ea254f40a0a57754
Andrew Emil <acemil@google.com>
2018-03-15 19:26:29 -0400

    Addressed futher code review comments from PR #80, as well as linting errors

--- test/configuration_unittest.cc
+++ test/configuration_unittest.cc
@@ -5,8 +5,8 @@ namespace google {
 
 class MetadataAgentConfigurationTest : public ::testing::Test {
  protected:
-  void ParseConfiguration(const std::string& configString) {
-    std::stringstream stream(configString);
+  void ParseConfiguration(const std::string& input) {
+    std::stringstream stream(input);
     config.ParseConfiguration(stream);
   }
 
@@ -21,17 +21,23 @@ class MetadataAgentConfigurationTest : public ::testing::Test {
     EXPECT_EQ(
         "https://stackdriver.googleapis.com/"
         "v1beta2/projects/{{project_id}}/resourceMetadata:batchUpdate",
-        config.MetadataIngestionEndpointFormat());
+        config.MetadataIngestionEndpointFormat()
+    );
     EXPECT_EQ(8*1024*1024, config.MetadataIngestionRequestSizeLimitBytes());
     EXPECT_EQ("0.1", config.MetadataIngestionRawContentVersion());
     EXPECT_EQ(60*60, config.InstanceUpdaterIntervalSeconds());
     EXPECT_EQ("", config.InstanceResourceType());
     EXPECT_EQ(60, config.DockerUpdaterIntervalSeconds());
-    EXPECT_EQ("unix://%2Fvar%2Frun%2Fdocker.sock/", config.DockerEndpointHost());
+    EXPECT_EQ(
+        "unix://%2Fvar%2Frun%2Fdocker.sock/",
+        config.DockerEndpointHost()
+    );
     EXPECT_EQ("1.23", config.DockerApiVersion());
     EXPECT_EQ("limit=30", config.DockerContainerFilter());
     EXPECT_EQ(0, config.KubernetesUpdaterIntervalSeconds());
-    EXPECT_EQ("https://kubernetes.default.svc", config.KubernetesEndpointHost());
+    EXPECT_EQ("https://kubernetes.default.svc",
+              config.KubernetesEndpointHost()
+    );
     EXPECT_EQ("", config.KubernetesPodLabelSelector());
     EXPECT_EQ("", config.KubernetesClusterName());
     EXPECT_EQ("", config.KubernetesClusterLocation());
@@ -44,41 +50,44 @@ class MetadataAgentConfigurationTest : public ::testing::Test {
   MetadataAgentConfiguration config;
 };
 
-TEST_F(MetadataAgentConfigurationTest, NoConfigTest) {
+TEST_F(MetadataAgentConfigurationTest, NoConfig) {
   VerifyDefaultConfig();
 }
 
-TEST_F(MetadataAgentConfigurationTest, EmptyConfigTest) {
+TEST_F(MetadataAgentConfigurationTest, EmptyConfig) {
   ParseConfiguration("");
   VerifyDefaultConfig();
 }
 
-TEST_F(MetadataAgentConfigurationTest, PopulatedConfigTest) {
+TEST_F(MetadataAgentConfigurationTest, PopulatedConfig) {
   ParseConfiguration(
       "ProjectId: TestProjectId\n"
       "MetadataApiNumThreads: 13\n"
-      "MetadataReporterPurgeDeleted: true");
+      "MetadataReporterPurgeDeleted: true"
+  );
   EXPECT_EQ("TestProjectId", config.ProjectId());
   EXPECT_EQ(13, config.MetadataApiNumThreads());
   EXPECT_EQ(true, config.MetadataReporterPurgeDeleted());
 }
 
-TEST_F(MetadataAgentConfigurationTest, CommentSkippedTest) {
+TEST_F(MetadataAgentConfigurationTest, CommentSkipped) {
   ParseConfiguration(
       "ProjectId: TestProjectId\n"
       "#MetadataApiNumThreads: 13\n"
-      "MetadataReporterPurgeDeleted: true");
+      "MetadataReporterPurgeDeleted: true"
+  );
   EXPECT_EQ(3, config.MetadataApiNumThreads());
 }
 
-TEST_F(MetadataAgentConfigurationTest, BlankLineTest) {
+TEST_F(MetadataAgentConfigurationTest, BlankLine) {
   ParseConfiguration(
       "ProjectId: TestProjectId\n"
       "\n"
       "\n"
-      "MetadataReporterPurgeDeleted: true");
+      "MetadataReporterPurgeDeleted: true"
+  );
   EXPECT_EQ("TestProjectId", config.ProjectId());
   EXPECT_EQ(true, config.MetadataReporterPurgeDeleted());
 }
 
-}  // google
+}  // namespace google
