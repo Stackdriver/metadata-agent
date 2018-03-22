b77b9630756d861ce1d9bdad71d90f3e265852fb
Andrew Emil <acemil@google.com>
2018-03-22 16:46:17 -0400

    Addressed missing comments on code review

--- src/health_checker.cc
+++ src/health_checker.cc
@@ -16,75 +16,52 @@
 
 #include "health_checker.h"
 
-#include <boost/algorithm/string/join.hpp>
 #include <boost/filesystem.hpp>
 #include <fstream>
-#include <iostream>
 #include <sstream>
 
 namespace google {
 
 HealthChecker::HealthChecker(const MetadataAgentConfiguration& config)
-    : health_states_({"kubernetes_pod_thread", "kubernetes_node_thread"}),
-      config_(config) {
+    : config_(config) {
+  InitialCreation();
   InitialCleanup();
 }
 
 void HealthChecker::SetUnhealthy(const std::string& state_name) {
-  TouchName(state_name);
+  if (health_files_.count(state_name) == 0) {
+    health_files_[state_name].reset(
+        new FileWrapper(config_.HealthCheckFileDirectory(), state_name));
+  }
   ReportHealth();
 }
 
 bool HealthChecker::ReportHealth() {
   if (!IsHealthy()) {
-    TouchName(config_.HealthCheckExternalFileName());
+    FileWrapper::Touch(config_.HealthCheckFileDirectory(),
+                       config_.HealthCheckExternalFileName());
     return false;
   }
   return true;
 }
 
 bool HealthChecker::IsHealthy() const {
-  for (const std::string& health_state : health_states_) {
-    if (CheckName(health_state)) {
+  for (int i = 0; i < sizeof(kHealthStates)/sizeof(char*); ++i) {
+    if (CheckName(kHealthStates[i])) {
       return false;
     }
   }
   return true;
 }
 
-std::string HealthChecker::MakeHealthCheckPath(const std::string& file_name) const {
-  if (!config_.HealthCheckFileDirectory().empty()) {
-    return boost::algorithm::join(
-        std::vector<std::string>{config_.HealthCheckFileDirectory(), file_name}, "/");
-  }
-  return file_name;
-}
-
-void HealthChecker::TouchName(const std::string& state_name) {
-  Touch(MakeHealthCheckPath(state_name));
-}
-
-void HealthChecker::Touch(const std::string& path) {
-  std::ofstream health_file(path);
-  health_file << std::endl;
-}
-
-bool HealthChecker::CheckName(const std::string& state_name) const {
-  return Check(MakeHealthCheckPath(state_name));
-}
-
-bool HealthChecker::Check(const std::string& path) const {
-  std::ifstream f(path);
-  return f.good();
+bool HealthChecker::CheckName(const std::string& name) const {
+  return FileWrapper::Exists(config_.HealthCheckFileDirectory(), name);
 }
 
 void HealthChecker::InitialCleanup() {
-  std::remove(MakeHealthCheckPath(config_.HealthCheckExternalFileName()).c_str());
-  for (const std::string& health_state : health_states_) {
-    std::remove(MakeHealthCheckPath(health_state).c_str());
-  }
+  FileWrapper::Remove(config_.HealthCheckFileDirectory(),
+                      config_.HealthCheckExternalFileName());
 }
-
 void HealthChecker::InitialCreation() {
   boost::filesystem::create_directories(config_.HealthCheckFileDirectory());
 }
--- src/health_checker.h
+++ src/health_checker.h
@@ -16,12 +16,57 @@
 #ifndef HEALTH_CHECKER_H_
 #define HEALTH_CHECKER_H_
 
+#include <boost/algorithm/string/join.hpp>
 #include <string>
 #include <vector>
+#include <map>
+#include <memory>
+#include <fstream>
 
 #include "configuration.h"
 
+namespace {
+  constexpr const char* kHealthStates[] = {
+    "kubernetes_pod_thread", "kubernetes_node_thread"
+  };
+}
 namespace google {
+
+class FileWrapper {
+ public:
+  FileWrapper(const std::string& directory, const std::string& name)
+       : directory_(directory), name_(name) {
+         Touch(directory_, name_);
+  }
+
+  static void Touch(const std::string& directory, const std::string& name) {
+    std::string path(boost::algorithm::join(
+        std::vector<std::string>{directory, name}, "/"));
+    std::ofstream health_file(path);
+    health_file << std::endl;
+  }
+
+  static void Remove(const std::string& directory, const std::string& name) {
+    std::string path(boost::algorithm::join(
+        std::vector<std::string>{directory, name}, "/"));
+    std::remove(path.c_str());
+  }
+
+  static bool Exists(const std::string& directory, const std::string& name) {
+    std::string path(boost::algorithm::join(
+        std::vector<std::string>{directory, name}, "/"));
+    std::ifstream f(path);
+    return f.good();
+  }
+
+  ~FileWrapper() {
+    FileWrapper::Remove(directory_, name_);
+  }
+
+  private:
+    const std::string& directory_;
+    const std::string& name_;
+};
 
 // Collects and reports health information about the metadata agent.
 class HealthChecker {
@@ -42,8 +87,8 @@ class HealthChecker {
   void InitialCleanup();
   void InitialCreation();
 
-  std::vector<std::string> health_states_;
   const MetadataAgentConfiguration& config_;
+  std::map<std::string, std::unique_ptr<FileWrapper>> health_files_;
 };
 
 }  // namespace google
