ccc382256ff719dd75d2283884eb01e10ea4688c
Bryan Moyles <bmoyles@google.com>
2018-03-26 22:56:50 +0200

    Add additional tests for ComputePodAssociations.

--- test/kubernetes_unittest.cc
+++ test/kubernetes_unittest.cc
@@ -8,7 +8,9 @@ namespace google {
 class KubernetesReaderTest : public ::testing::Test {
  protected:
   void InitializeReader() {
-    std::stringstream stream("");
+    std::stringstream stream(
+      "InstanceId: instance-1\n"
+      "InstanceZone: us-east1-d\n");
     config.ParseConfiguration(stream);
 
     reader.reset(new KubernetesReader(config));
@@ -74,6 +76,72 @@ TEST_F(KubernetesReaderTest, ComputePodAssociationsPodWithoutMetadataUidThrowsEx
       FAIL() << "Expected an error complaining about the missing key in '" 
              << err.what() << "'";
     }
+  }
+}
+
+TEST_F(KubernetesReaderTest, ComputePodAssociationsPodWithoutMetadataNameThrowsException) {
+  InitializeReader();
+
+  try {
+    ComputePodAssociations(new json::Object({
+      {"metadata", json::object({
+        {"namespace", json::string("default")},
+        {"uid", json::string("12345678")},
+      })},
+    }));
+    FAIL() << "Expected json::Exception";
+  } catch(json::Exception const& err) {
+    if (err.what().find("There is no name") != 0) {
+      FAIL() << "Expected an error complaining about the missing key in '" 
+             << err.what() << "'";
+    }
+  }
+}
+
+TEST_F(KubernetesReaderTest, ComputePodAssociationsPodWithoutSpecThrowsException) {
+  InitializeReader();
+
+  try {
+    ComputePodAssociations(new json::Object({
+      {"metadata", json::object({
+        {"namespace", json::string("default")},
+        {"uid", json::string("12345678")},
+        {"name", json::string("some-random-pod")},
+      })},
+    }));
+    FAIL() << "Expected json::Exception";
+  } catch(json::Exception const& err) {
+    if (err.what().find("There is no spec") != 0) {
+      FAIL() << "Expected an error complaining about the missing key in '" 
+             << err.what() << "'";
+    }
+  }
+}
+
+TEST_F(KubernetesReaderTest, ComputePodAssociationsPodValidInput) {
+  InitializeReader();
+
+  const std::string podName("some-random-pod");
+
+  std::string podAssociations(ComputePodAssociations(new json::Object({
+    {"metadata", json::object({
+      {"namespace", json::string("default")},
+      {"uid", json::string("12345678")},
+      {"name", json::string(podName)},
+    })},
+    {"spec", json::object({})},
+  }))->ToString());
+
+  if (podAssociations.find("Pod") == std::string::npos) {
+    FAIL() << "Pod should be somewhere in the pod association.";
+  }
+
+  if (podAssociations.find(podName) == std::string::npos) {
+    FAIL() << "The pod's name should be somewhere in the pod association.";
+  }
+
+  if (podAssociations.find("gce_instance") == std::string::npos) {
+    FAIL() << "The pod's name should be somewhere in the pod association.";
   }
 }
 
