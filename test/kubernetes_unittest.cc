b0a054180a755cb5a4f370f32fad492fca012890
Andrew Emil <acemil@google.com>
2018-03-29 15:22:12 -0400

    Addressed code review comments

--- test/kubernetes_unittest.cc
+++ test/kubernetes_unittest.cc
@@ -1,7 +1,7 @@
-#include "../src/kubernetes.h"
-#include "../src/updater.h"
 #include "../src/configuration.h"
 #include "../src/instance.h"
+#include "../src/kubernetes.h"
+#include "../src/updater.h"
 #include "gtest/gtest.h"
 
 namespace google {
@@ -18,36 +18,34 @@ class KubernetesTest : public ::testing::Test {
 
 TEST_F(KubernetesTest, GetNodeMetadata) {
   Configuration config(std::stringstream(
-    "KubernetesClusterName: 'TestClusterName'\n"
-    "KubernetesClusterLocation: 'TestClusterLocation'\n"
-    "MetadataIngestionRawContentVersion: 'TestVersion'\n"
-    "InstanceZone: 'TestZone'\n"
-    "InstanceId: 'TestID'\n"
+    "KubernetesClusterName: TestClusterName\n"
+    "KubernetesClusterLocation: TestClusterLocation\n"
+    "MetadataIngestionRawContentVersion: TestVersion\n"
+    "InstanceZone: TestZone\n"
+    "InstanceId: TestID\n"
   ));
   Environment environment(config);
   KubernetesReader reader(config, nullptr);
-  json::value node_json = json::object({
+  json::value node = json::object({
     {"metadata", json::object({
       {"name", json::string("testname")},
-      {"creationTimestamp", json::string("2018-03-03T01:23:45.678901234Z")}
+      {"creationTimestamp", json::string("2018-03-03T01:23:45.678901234Z")},
     })}
   });
-  const json::Object* node = node_json->As<json::Object>();
-  MetadataUpdater::ResourceMetadata metadata(
-      GetNodeMetadata(reader, node, Timestamp(), false));
-  EXPECT_EQ(metadata.ids.size(), 1);
-  EXPECT_EQ(metadata.ids[0], "k8s_node.testname");
-  EXPECT_EQ(metadata.resource,
-            MonitoredResource("k8s_node", {
-                                {"cluster_name", "TestClusterName"},
-                                {"node_name", "testname"},
-                                {"location", "TestClusterLocation"}
-                              }));
-  EXPECT_EQ(metadata.metadata.version, "TestVersion");
-  EXPECT_EQ(metadata.metadata.is_deleted, false);
-  EXPECT_EQ(metadata.metadata.created_at,
-            time::rfc3339::FromString("2018-03-03T01:23:45.678901234Z"));
-  EXPECT_EQ(metadata.metadata.collected_at, Timestamp());
+  const auto metadata(
+      GetNodeMetadata(reader, node->As<json::Object>(), Timestamp(), false));
+  EXPECT_EQ(1, metadata.ids.size());
+  EXPECT_EQ("k8s_node.testname", metadata.ids[0]);
+  EXPECT_EQ(MonitoredResource("k8s_node", {
+    {"cluster_name", "TestClusterName"},
+    {"node_name", "testname"},
+    {"location", "TestClusterLocation"},
+  }), metadata.resource);
+  EXPECT_EQ("TestVersion", metadata.metadata.version);
+  EXPECT_EQ(false, metadata.metadata.is_deleted);
+  EXPECT_EQ(time::rfc3339::FromString("2018-03-03T01:23:45.678901234Z"),
+            metadata.metadata.created_at);
+  EXPECT_EQ(Timestamp(), metadata.metadata.collected_at);
   json::value big = json::object({
     {"blobs", json::object({
       {"association", json::object({
@@ -58,13 +56,11 @@ TEST_F(KubernetesTest, GetNodeMetadata) {
         })},
       })},
       {"api", json::object({
-        // 1.6 is hardcoded as kKubernetesApiVersion
-        {"version", json::string("1.6")},
-        {"raw", node->Clone()},
+        {"version", json::string("1.6")},  // Hard-coded in kubernetes.cc
+        {"raw", std::move(node)},
       })},
     })},
   });
-  EXPECT_EQ(big->As<json::Object>()->ToString(),
-            metadata.metadata.metadata->As<json::Object>()->ToString());
+  EXPECT_EQ(big->ToString(), metadata.metadata.metadata->ToString());
 }
-} // namespace google
+}  // namespace google
