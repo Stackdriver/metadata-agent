080d0eb6c05d558ed4be53fb0ce54a7736c31c6c
Andrew Emil <acemil@google.com>
2018-03-30 14:04:47 -0400

    Added test for ComputePodAssociations

--- test/kubernetes_unittest.cc
+++ test/kubernetes_unittest.cc
@@ -1,3 +1,5 @@
+#include <boost/algorithm/string/join.hpp>
+
 #include "../src/configuration.h"
 #include "../src/kubernetes.h"
 #include "../src/updater.h"
@@ -12,6 +14,16 @@ class KubernetesTest : public ::testing::Test {
       Timestamp collected_at, bool is_deleted) const
       throw(json::Exception) {
     return reader.GetNodeMetadata(node, collected_at, is_deleted);
+  }
+
+  json::value ComputePodAssociations(const json::Object* pod,
+				     const KubernetesReader& reader) {
+    return reader.ComputePodAssociations(pod);
+  }
+
+  void InsertIntoOwners(const std::string& key, json::value& value,
+			KubernetesReader* reader) {
+    reader->owners_[key] = value->Clone();
   }
 };
 
@@ -67,5 +79,68 @@ TEST_F(KubernetesTest, GetNodeMetadata) {
     })},
   });
   EXPECT_EQ(big->ToString(), m.metadata.metadata->ToString());
+}
+
+TEST_F(KubernetesTest, ComputePodAssociations) {
+  const std::string api_version = "1.2.3";
+  const std::string kind = "TestKind";
+  const std::string uid1 = "TestUID1";
+  Configuration config(std::stringstream(
+    "KubernetesClusterName: TestClusterName\n"
+    "KubernetesClusterLocation: TestClusterLocation\n"
+    "MetadataIngestionRawContentVersion: TestVersion\n"
+    "InstanceZone: TestZone\n"
+    "InstanceId: TestID\n"
+  ));
+  Environment environment(config);
+  KubernetesReader reader(config, nullptr);  // Don't need HealthChecker.
+  const std::string encoded_ref = boost::algorithm::join(
+      std::vector<std::string>{api_version, kind, uid1}, "/");
+  json::value controller = json::object({
+          {"controller", json::boolean(true)},
+          {"apiVersion", json::string(api_version)},
+          {"kind", json::string(kind)},
+          {"name", json::string("TestName")},
+          {"uid", json::string(uid1)},
+	  {"metadata", json::object({
+	    {"name", json::string("InnerTestName")},
+	    {"kind", json::string("InnerTestKind")},
+	    {"uid", json::string("InnerTestUID1")},
+	  })},
+  });
+  InsertIntoOwners(encoded_ref, controller, &reader);
+  json::value pod = json::object({
+    {"metadata", json::object({
+      {"namespace", json::string("TestNamespace")},
+      {"uid", json::string("TestUID0")},
+      {"ownerReferences", json::array({
+        json::object({{"no_controller", json::boolean(true)}}),
+	controller->Clone(),
+      })},
+    })},
+    {"spec", json::object({
+      {"nodeName", json::string("TestSpecNodeName")},
+    })},
+  });
+
+  auto associations = ComputePodAssociations(pod->As<json::Object>(), reader);
+
+  EXPECT_EQ(associations->ToString(), json::object({
+    {"raw", json::object({
+      {"controllers", json::object({
+	{"topLevelControllerName", json::string("InnerTestName")},
+	{"topLevelControllerType", json::string(kind)},
+      })},
+      {"infrastructureResource", json::object({
+	{"labels", json::object({
+	  {"instance_id", json::string("TestID")},
+	  {"zone", json::string("TestZone")}
+	})},
+	{"type", json::string("gce_instance")},
+      })},
+      {"nodeName", json::string("TestSpecNodeName")},
+    })},
+    {"version", json::string("TestVersion")},
+  })->ToString());
 }
 }  // namespace google
