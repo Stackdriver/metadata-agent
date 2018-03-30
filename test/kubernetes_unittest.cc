f6b9b597968aeaf67c4f0656b3e24a8ab02661b6
ACEmilG <37227825+ACEmilG@users.noreply.github.com>
2018-03-30 10:59:39 -0400

    Add test for GetNodeMetadata in kubernetes (#102)

---
+++ test/kubernetes_unittest.cc
@@ -0,0 +1,71 @@
+#include "../src/configuration.h"
+#include "../src/kubernetes.h"
+#include "../src/updater.h"
+#include "gtest/gtest.h"
+
+namespace google {
+
+class KubernetesTest : public ::testing::Test {
+ protected:
+  MetadataUpdater::ResourceMetadata GetNodeMetadata(
+      const KubernetesReader& reader, const json::Object *node,
+      Timestamp collected_at, bool is_deleted) const
+      throw(json::Exception) {
+    return reader.GetNodeMetadata(node, collected_at, is_deleted);
+  }
+};
+
+TEST_F(KubernetesTest, GetNodeMetadata) {
+  Configuration config(std::stringstream(
+    "KubernetesClusterName: TestClusterName\n"
+    "KubernetesClusterLocation: TestClusterLocation\n"
+    "MetadataIngestionRawContentVersion: TestVersion\n"
+    "InstanceResourceType: gce_instance\n"
+    "InstanceZone: TestZone\n"
+    "InstanceId: TestID\n"
+  ));
+  Environment environment(config);
+  KubernetesReader reader(config, nullptr);  // Don't need HealthChecker.
+  json::value node = json::object({
+    {"metadata", json::object({
+      {"name", json::string("testname")},
+      {"creationTimestamp", json::string("2018-03-03T01:23:45.678901234Z")},
+    })}
+  });
+  const auto m =
+      GetNodeMetadata(reader, node->As<json::Object>(), Timestamp(), false);
+  EXPECT_EQ(1, m.ids.size());
+  EXPECT_EQ("k8s_node.testname", m.ids[0]);
+  EXPECT_EQ(MonitoredResource("k8s_node", {
+    {"cluster_name", "TestClusterName"},
+    {"node_name", "testname"},
+    {"location", "TestClusterLocation"},
+  }), m.resource);
+  EXPECT_EQ("TestVersion", m.metadata.version);
+  EXPECT_EQ(false, m.metadata.is_deleted);
+  EXPECT_EQ(time::rfc3339::FromString("2018-03-03T01:23:45.678901234Z"),
+            m.metadata.created_at);
+  EXPECT_EQ(Timestamp(), m.metadata.collected_at);
+  json::value big = json::object({
+    {"blobs", json::object({
+      {"association", json::object({
+        {"version", json::string("TestVersion")},
+        {"raw", json::object({
+          {"infrastructureResource", json::object({
+            {"type", json::string("gce_instance")},
+            {"labels", json::object({
+              {"instance_id", json::string("TestID")},
+              {"zone", json::string("TestZone")},
+            })},
+          })},
+        })},
+      })},
+      {"api", json::object({
+        {"version", json::string("1.6")},  // Hard-coded in kubernetes.cc.
+        {"raw", std::move(node)},
+      })},
+    })},
+  });
+  EXPECT_EQ(big->ToString(), m.metadata.metadata->ToString());
+}
+}  // namespace google
