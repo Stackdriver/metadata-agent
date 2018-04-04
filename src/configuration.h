01b0082d0423ba9bb3975c003a5cc2f1095cadbd
Supriya Garg <supriyagarg@users.noreply.github.com>
2018-04-04 18:06:19 -0400

    Split the metadata API request when it hits a limit by count or byte size (#129)
    * Add a flag for the limit of the request size by count.
    
    * Update SendMetadata to split the request on hitting the count limit
    
    * Address comments

--- src/configuration.h
+++ src/configuration.h
@@ -77,6 +77,10 @@ class Configuration {
     std::lock_guard<std::mutex> lock(mutex_);
     return metadata_ingestion_request_size_limit_bytes_;
   }
+  int MetadataIngestionRequestSizeLimitCount() const {
+    std::lock_guard<std::mutex> lock(mutex_);
+    return metadata_ingestion_request_size_limit_count_;
+  }
   const std::string& MetadataIngestionRawContentVersion() const {
     std::lock_guard<std::mutex> lock(mutex_);
     return metadata_ingestion_raw_content_version_;
@@ -183,6 +187,7 @@ class Configuration {
   std::string metadata_reporter_user_agent_;
   std::string metadata_ingestion_endpoint_format_;
   int metadata_ingestion_request_size_limit_bytes_;
+  int metadata_ingestion_request_size_limit_count_;
   std::string metadata_ingestion_raw_content_version_;
   int instance_updater_interval_seconds_;
   std::string instance_resource_type_;
--- test/configuration_unittest.cc
+++ test/configuration_unittest.cc
@@ -18,6 +18,7 @@ void VerifyDefaultConfig(const Configuration& config) {
             "v1beta2/projects/{{project_id}}/resourceMetadata:batchUpdate",
             config.MetadataIngestionEndpointFormat());
   EXPECT_EQ(8*1024*1024, config.MetadataIngestionRequestSizeLimitBytes());
+  EXPECT_EQ(1000, config.MetadataIngestionRequestSizeLimitCount());
   EXPECT_EQ("0.1", config.MetadataIngestionRawContentVersion());
   EXPECT_EQ(60*60, config.InstanceUpdaterIntervalSeconds());
   EXPECT_EQ("", config.InstanceResourceType());
@@ -56,12 +57,14 @@ TEST(ConfigurationTest, PopulatedConfig) {
       "MetadataReporterPurgeDeleted: true\n"
       "MetadataReporterUserAgent: \"foobar/foobaz\"\n"
       "HealthCheckFile: /a/b/c\n"
+      "MetadataIngestionRequestSizeLimitCount: 500\n"
   ));
   EXPECT_EQ("TestProjectId", config.ProjectId());
   EXPECT_EQ(13, config.MetadataApiNumThreads());
   EXPECT_EQ(true, config.MetadataReporterPurgeDeleted());
   EXPECT_EQ("foobar/foobaz", config.MetadataReporterUserAgent());
   EXPECT_EQ("/a/b/c", config.HealthCheckFile());
+  EXPECT_EQ(500, config.MetadataIngestionRequestSizeLimitCount());
 }
 
 TEST(ConfigurationTest, CommentSkipped) {
