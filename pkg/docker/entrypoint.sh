89edd20bddea4ecd2905a5df930aaf21150be24f
bmoyles0117 <bmoy117@gmail.com>
2018-05-22 16:43:44 -0400

    Add entrypoint.sh to abstract around metadatad binary. (#40)

---
+++ pkg/docker/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -e
+
+# This docker image supports sending either a flag or a command as the docker
+# command. When a flag is sent, it will be passed on to the metadata agent 
+# process. Anything else will be interpreted as the command to be run.
+#
+# Passing a flag.
+# $ docker run -it {image:tag} -v
+#
+# Passing a command.
+# $ docker run -it {image:tag} /bin/bash
+#
+# Default behavior uses CMD defined in Dockerfile.
+# $ docker run -it {image:tag}
+
+if [ "${1:0:1}" = '-' ]; then
+  set -- /opt/stackdriver/metadata/sbin/metadatad "$@"
+fi
+
+exec "$@"
