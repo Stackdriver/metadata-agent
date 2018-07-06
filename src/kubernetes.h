ec8434e022c2458a6c3af2c00c69798342b468b6
Supriya Garg <supriyagarg@google.com>
2018-07-06 15:41:30 -0400

    Update the kubernetes code to generate the required fields for v1beta3.

--- src/kubernetes.h
+++ src/kubernetes.h
@@ -76,6 +76,10 @@ class KubernetesReader {
   json::value QueryMaster(const std::string& path) const
       throw(QueryException, json::Exception);
 
+  // Builds the cluster full name by reading in cluster related environment
+  // variables.
+  const std::string ClusterFullName() const;
+
   // Issues a Kubernetes master API query at a given path and
   // watches for parsed JSON responses. The path has to start with "/".
   // Invokes callback for every notification.
