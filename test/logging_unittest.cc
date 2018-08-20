4d8fd568e2dc3fb0aa2e23a02f68e3a427f954af
davidbtucker <39198191+davidbtucker@users.noreply.github.com>
2018-08-20 12:42:04 -0400

    Unit tests for logging. (#181)

---
+++ test/logging_unittest.cc
@@ -0,0 +1,26 @@
+#include "../src/logging.h"
+#include "gtest/gtest.h"
+
+#include <regex>
+#include <sstream>
+
+namespace google {
+namespace {
+
+TEST(LoggingTest, Logger) {
+  std::ostringstream out;
+
+  // Logger flushes on destruction.
+  {
+    LogStream stream(out);
+    Logger logger("somefile.cc", 123, Logger::WARNING, &stream);
+    logger << "Test message";
+  }
+
+  EXPECT_TRUE(std::regex_match(out.str(), std::regex(
+     "W\\d{4} \\d{2}:\\d{2}:\\d{2} 0x[0-9a-f]{12} "
+     "somefile.cc:123 Test message\n")));
+}
+
+}  // namespace
+}  // namespace google
