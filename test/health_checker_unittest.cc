bae83342f16877bceae85aecace88eee38dce28b
Igor Peshansky <igorp@google.com>
2018-09-13 17:59:17 -0400

    Add tests for metadata staleness checks.

--- test/health_checker_unittest.cc
+++ test/health_checker_unittest.cc
@@ -1,5 +1,6 @@
 #include "../src/health_checker.h"
 #include "../src/store.h"
+#include "../src/time.h"
 #include "gtest/gtest.h"
 #include <sstream>
 
@@ -65,6 +66,62 @@ TEST_F(HealthCheckerUnittest, FailurePersists) {
             std::set<std::string>({
                 "kubernetes_pod_thread",
             }));
+}
+
+TEST_F(HealthCheckerUnittest, RecentMetadataSucceeds) {
+  Configuration config(std::istringstream(
+      "HealthCheckMaxDataAgeSeconds: 20"
+  ));
+  MetadataStore store(config);
+  HealthChecker health_checker(config, store);
+  EXPECT_TRUE(health_checker.UnhealthyComponents().empty());
+  time_point now = std::chrono::system_clock::now();
+  time_point then = now - std::chrono::seconds(10);
+  store.UpdateMetadata(
+      MonitoredResource("my_resource", {}),
+      MetadataStore::Metadata("0", false, then, then, json::object({})));
+  EXPECT_TRUE(health_checker.UnhealthyComponents().empty());
+}
+
+TEST_F(HealthCheckerUnittest, StaleMetadataCausesFailure) {
+  Configuration config(std::istringstream(
+      "HealthCheckMaxDataAgeSeconds: 1"
+  ));
+  MetadataStore store(config);
+  HealthChecker health_checker(config, store);
+  EXPECT_TRUE(health_checker.UnhealthyComponents().empty());
+  time_point now = std::chrono::system_clock::now();
+  time_point then = now - std::chrono::seconds(10);
+  store.UpdateMetadata(
+      MonitoredResource("my_resource", {}),
+      MetadataStore::Metadata("0", false, then, then, json::object({})));
+  EXPECT_EQ(health_checker.UnhealthyComponents(),
+            std::set<std::string>({
+                "my_resource",
+            }));
+}
+
+TEST_F(HealthCheckerUnittest, UpdatedMetadataClearsFailure) {
+  Configuration config(std::istringstream(
+      "HealthCheckMaxDataAgeSeconds: 1"
+  ));
+  MetadataStore store(config);
+  HealthChecker health_checker(config, store);
+  EXPECT_TRUE(health_checker.UnhealthyComponents().empty());
+  time_point now = std::chrono::system_clock::now();
+  time_point then = now - std::chrono::seconds(10);
+  store.UpdateMetadata(
+      MonitoredResource("my_resource", {}),
+      MetadataStore::Metadata("0", false, then, then, json::object({})));
+  EXPECT_EQ(health_checker.UnhealthyComponents(),
+            std::set<std::string>({
+                "my_resource",
+            }));
+  now = std::chrono::system_clock::now();
+  store.UpdateMetadata(
+      MonitoredResource("my_resource", {}),
+      MetadataStore::Metadata("0", false, now, now, json::object({})));
+  EXPECT_TRUE(health_checker.UnhealthyComponents().empty());
 }
 
 }  // namespace google
